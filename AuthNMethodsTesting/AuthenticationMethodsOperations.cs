d0aeac94a6347400d3fa2774a7f07ac05b566a7d
Kalyan Krishna <kalyan.krishna@microsoft.com>
2020-09-24 22:31:03 -0700

    more AuthN methods refactoring

--- AuthNMethodsTesting/AuthenticationMethodsOperations.cs
+++ AuthNMethodsTesting/AuthenticationMethodsOperations.cs
@@ -2,11 +2,10 @@
 
 using AuthNMethodsTesting.Model;
 using Common;
+using Microsoft.Graph;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -53,7 +52,7 @@ namespace AuthNMethodsTesting
 
         public async Task<temporaryAccesPass> GenerateTemporaryAccessPassForUser(Beta.User user, int lifetimeInMinutes)
         {
-            var requestUrl = $"https://graph.microsoft.com/beta/users/{user.Id}/authentication/temporaryAccessPassMethods";            
+            var requestUrl = $"https://graph.microsoft.com/beta/users/{user.Id}/authentication/temporaryAccessPassMethods";
             HttpClient httpClient = await _graphServiceClient.GetHttpClientForMSGraphAsync();
 
             HttpContent content = new StringContent(JsonConvert.SerializeObject(new temporaryAccessPassRequest() { lifetimeInMinutes = lifetimeInMinutes }));
@@ -106,7 +105,7 @@ namespace AuthNMethodsTesting
                 toreturn.AppendLine($"Id:{tap.Id}");
                 toreturn.AppendLine($"TemporaryAccessPass:{tap.TemporaryAccessPass}");
                 toreturn.AppendLine($"LifetimeInMinutes:{tap.LifetimeInMinutes}");
-                toreturn.AppendLine($"StartDateTime:{tap.StartDateTime}");                
+                toreturn.AppendLine($"StartDateTime:{tap.StartDateTime}");
             }
 
             return toreturn.ToString();
@@ -143,40 +142,201 @@ namespace AuthNMethodsTesting
             return toreturn.ToString();
         }
 
-        public async Task GetUsersAuthenticationMethodsAsync()
+        public async Task<List<Beta.AuthenticationMethod>> GetUsersAuthenticationMethodsAsync()
         {
-            var requestUrl = "https://graph.microsoft.com/beta/me/authentication/methods";
-            HttpHelper httpHelper = new HttpHelper(new ColorConsoleLogger());
-            HttpClient httpClient = await _graphServiceClient.GetHttpClientForMSGraphAsync();
+            var methods = await _graphServiceClient.Me.Authentication.Methods.Request().GetAsync();
 
-            HttpResponseMessage rawResponse = await httpHelper.GetRawHttpResponseAsync(httpClient, async client => await client.GetAsync(requestUrl));
+            return await ProcessIAuthenticationMethodsCollectionPage(methods);
 
-            string jsonresponse = rawResponse.ProcessHttpResponse();
+            //var requestUrl = "https://graph.microsoft.com/beta/me/authentication/methods";
+            //HttpHelper httpHelper = new HttpHelper(new ColorConsoleLogger());
+            //HttpClient httpClient = await _graphServiceClient.GetHttpClientForMSGraphAsync();
 
-            JObject callresults = JObject.Parse(jsonresponse);
-            // get JSON result objects into a list
-            IList<JToken> results = callresults["value"].Children().ToList();
+            //HttpResponseMessage rawResponse = await httpHelper.GetRawHttpResponseAsync(httpClient, async client => await client.GetAsync(requestUrl));
 
-            authenticationMethod authenticationMethod = results[0].ToObject<authenticationMethod>();
-            ColorConsole.WriteLine(ConsoleColor.Green, $"id-{authenticationMethod.id}, isUsable-{authenticationMethod.isUsable}, phoneNumber-{authenticationMethod.phoneNumber}");
+            //string jsonresponse = rawResponse.ProcessHttpResponse();
+
+            //JObject callresults = JObject.Parse(jsonresponse);
+            //// get JSON result objects into a list
+            //IList<JToken> results = callresults["value"].Children().ToList();
+
+            //authenticationMethod authenticationMethod = results[0].ToObject<authenticationMethod>();
+            //ColorConsole.WriteLine(ConsoleColor.Green, $"id-{authenticationMethod.id}, isUsable-{authenticationMethod.isUsable}, phoneNumber-{authenticationMethod.phoneNumber}");
         }
 
-        public async Task GetUsersPhoneMethodsAsync()
+        private async Task<List<Beta.AuthenticationMethod>> ProcessIAuthenticationMethodsCollectionPage(Beta.IAuthenticationMethodsCollectionPage authenticationMethodsPage)
         {
-            var requestUrl = "https://graph.microsoft.com/beta/me/authentication/phoneMethods";
-            HttpHelper httpHelper = new HttpHelper(new ColorConsoleLogger());
-            HttpClient httpClient = await _graphServiceClient.GetHttpClientForMSGraphAsync();
+            List<Beta.AuthenticationMethod> allMethods = new List<Beta.AuthenticationMethod>();
 
-            HttpResponseMessage rawResponse = await httpHelper.GetRawHttpResponseAsync(httpClient, async client => await client.GetAsync(requestUrl));
+            try
+            {
+                if (authenticationMethodsPage != null)
+                {
+                    do
+                    {
+                        // Page through results
+                        foreach (var authenticationMethod in authenticationMethodsPage.CurrentPage)
+                        {
+                            allMethods.Add(authenticationMethod);
+                        }
 
-            string jsonresponse = rawResponse.ProcessHttpResponse();
+                        // are there more pages (Has a @odata.nextLink ?)
+                        if (authenticationMethodsPage.NextPageRequest != null)
+                        {
+                            authenticationMethodsPage = await authenticationMethodsPage.NextPageRequest.GetAsync();
+                        }
+                        else
+                        {
+                            authenticationMethodsPage = null;
+                        }
+                    } while (authenticationMethodsPage != null);
+                }
+            }
+            catch (ServiceException e)
+            {
+                Console.WriteLine($"We could not process the authentication methods list: {e}");
+                return null;
+            }
 
-            JObject callresults = JObject.Parse(jsonresponse);
-            // get JSON result objects into a list
-            IList<JToken> results = callresults["value"].Children().ToList();
+            return allMethods;
+        }
 
-            phoneAuthenticationMethod phoneMethod = results[0].ToObject<phoneAuthenticationMethod>();
-            ColorConsole.WriteLine(ConsoleColor.Green, $"phoneType-{phoneMethod.phoneType}, phoneNumber-{phoneMethod.phoneNumber}, smsSignInState-{phoneMethod.smsSignInState}");
+        public string PrintAuthenticationMethod(Beta.AuthenticationMethod method)
+        {
+            StringBuilder toreturn = new StringBuilder();
+
+            if (method == null)
+            {
+                toreturn.Append("The provided authN method object is null");
+            }
+            else
+            {
+                toreturn.AppendLine($"Id:{method.Id}");
+
+                if (method is Beta.PhoneAuthenticationMethod)
+                {
+                    Beta.PhoneAuthenticationMethod phoneAuth = method as Beta.PhoneAuthenticationMethod;
+
+                    toreturn.AppendLine($"PhoneNumber:{phoneAuth.PhoneNumber}");
+                    toreturn.AppendLine($"PhoneType:{phoneAuth.PhoneType}");
+                    toreturn.AppendLine($"SmsSignInState:{phoneAuth.SmsSignInState}");
+                }
+                else if (method is Beta.PasswordAuthenticationMethod)
+                {
+                    Beta.PasswordAuthenticationMethod passwordAuthenticationMethod = method as Beta.PasswordAuthenticationMethod;
+
+                    toreturn.AppendLine($"Id:{passwordAuthenticationMethod.Id}");
+                    toreturn.AppendLine($"Password:{passwordAuthenticationMethod.Password}");
+                    toreturn.AppendLine($"CreationDateTime:{passwordAuthenticationMethod.CreationDateTime}");
+                }
+                else
+                {
+                    ColorConsole.WriteLine(ConsoleColor.Red, $"No provision in PrintAuthenticationMethod for type-{method.ODataType}");
+                }
+            }
+
+            return toreturn.ToString();
+        }
+
+        public async Task<List<Beta.AuthenticationMethod>> ListMyPhoneAuthenticationMethodsAsync()
+        {
+            var phoneAuthMethods = await _graphServiceClient.Me.Authentication.PhoneMethods.Request().GetAsync();
+
+            return await ProcessIAuthenticationMethodsCollectionPage(phoneAuthMethods as Beta.IAuthenticationMethodsCollectionPage);
+            //return await ProcessIAuthenticationPhoneMethodsCollectionPage(phoneAuthMethods);
+
+            //var requestUrl = "https://graph.microsoft.com/beta/me/authentication/phoneMethods";
+            //HttpHelper httpHelper = new HttpHelper(new ColorConsoleLogger());
+            //HttpClient httpClient = await _graphServiceClient.GetHttpClientForMSGraphAsync();
+
+            //HttpResponseMessage rawResponse = await httpHelper.GetRawHttpResponseAsync(httpClient, async client => await client.GetAsync(requestUrl));
+
+            //string jsonresponse = rawResponse.ProcessHttpResponse();
+
+            //JObject callresults = JObject.Parse(jsonresponse);
+            //// get JSON result objects into a list
+            //IList<JToken> results = callresults["value"].Children().ToList();
+
+            //phoneAuthenticationMethod phoneMethod = results[0].ToObject<phoneAuthenticationMethod>();
+            //ColorConsole.WriteLine(ConsoleColor.Green, $"phoneType-{phoneMethod.phoneType}, phoneNumber-{phoneMethod.phoneNumber}, smsSignInState-{phoneMethod.smsSignInState}");
+        }
+
+        public async Task<Beta.AuthenticationMethod> ListMyPhoneAuthenticationMethodAsync(string phoneAuthenticationMethodId)
+        {
+            return await _graphServiceClient.Me.Authentication.PhoneMethods[phoneAuthenticationMethodId].Request().GetAsync();
+        }
+
+        public async Task<List<Beta.AuthenticationMethod>> ListUsersPhoneAuthenticationMethodsAsync(string userId)
+        {
+            var phoneAuthMethods = await _graphServiceClient.Users[userId].Authentication.PhoneMethods.Request().GetAsync();
+
+            return await ProcessIAuthenticationMethodsCollectionPage(phoneAuthMethods as Beta.IAuthenticationMethodsCollectionPage);
+        }
+
+        public async Task<Beta.AuthenticationMethod> ListUsersPhoneAuthenticationMethodAsync(string userId, string phoneAuthenticationMethodId)
+        {
+            return await _graphServiceClient.Users[userId].Authentication.PhoneMethods[phoneAuthenticationMethodId].Request().GetAsync();
+        }
+
+        public async Task<List<Beta.AuthenticationMethod>> ListMyPasswordAuthenticationMethodsAsync()
+        {
+            var passwordAuthMethods = await _graphServiceClient.Me.Authentication.PasswordMethods.Request().GetAsync();
+
+            return await ProcessIAuthenticationMethodsCollectionPage(passwordAuthMethods as Beta.IAuthenticationMethodsCollectionPage);
+        }
+
+        public async Task<Beta.AuthenticationMethod> ListMyPasswordAuthenticationMethodAsync(string passwordAuthenticationMethodId)
+        {
+            return await _graphServiceClient.Me.Authentication.PasswordMethods[passwordAuthenticationMethodId].Request().GetAsync();
+        }
+
+        public async Task<List<Beta.AuthenticationMethod>> ListUsersPasswordAuthenticationMethodsAsync(string userId)
+        {
+            var passwordAuthMethods = await _graphServiceClient.Users[userId].Authentication.PasswordMethods.Request().GetAsync();
+
+            return await ProcessIAuthenticationMethodsCollectionPage(passwordAuthMethods as Beta.IAuthenticationMethodsCollectionPage);
+        }
+
+        public async Task<Beta.AuthenticationMethod> ListUsersPasswordAuthenticationMethodAsync(string userId, string passwordAuthenticationMethodId)
+        {
+            return await _graphServiceClient.Users[userId].Authentication.PasswordMethods[passwordAuthenticationMethodId].Request().GetAsync();
+        }
+
+        private async Task<List<Beta.AuthenticationMethod>> ProcessIAuthenticationPhoneMethodsCollectionPage(Beta.IAuthenticationPhoneMethodsCollectionPage authenticationPhoneMethodsPage)
+        {
+            List<Beta.AuthenticationMethod> allMethods = new List<Beta.AuthenticationMethod>();
+
+            try
+            {
+                if (authenticationPhoneMethodsPage != null)
+                {
+                    do
+                    {
+                        // Page through results
+                        foreach (var authenticationMethod in authenticationPhoneMethodsPage.CurrentPage)
+                        {
+                            allMethods.Add(authenticationMethod);
+                        }
+
+                        // are there more pages (Has a @odata.nextLink ?)
+                        if (authenticationPhoneMethodsPage.NextPageRequest != null)
+                        {
+                            authenticationPhoneMethodsPage = await authenticationPhoneMethodsPage.NextPageRequest.GetAsync();
+                        }
+                        else
+                        {
+                            authenticationPhoneMethodsPage = null;
+                        }
+                    } while (authenticationPhoneMethodsPage != null);
+                }
+            }
+            catch (ServiceException e)
+            {
+                Console.WriteLine($"We could not process the phone authentication methods list: {e}");
+                return null;
+            }
+
+            return allMethods;
         }
     }
 }
